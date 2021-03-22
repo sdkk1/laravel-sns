f15080865c7e186d9564618a86a995c376b273d2
Keita Nishimura <fast.break.baske03@gmail.com>
2021-03-22 13:45:33 +0900

    feat:ナビバーの表示をログイン前後で変わる仕様を追加

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,16 +1,18 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
   <ul class="navbar-nav ml-auto">
+    @guest
     <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="">ログイン</a>
     </li>
+    @endguest
+    @auth
     <li class="nav-item">
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
-    <!-- Dropdown -->
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
          aria-haspopup="true" aria-expanded="false">
@@ -27,8 +29,9 @@
         </button>
       </div>
     </li>
-    <form id="logout-button" method="POST" action="">
+    <form id="logout-button" method="POST" action="{{ route('logout') }}">
+      @csrf
     </form>
-    <!-- Dropdown -->
+    @endauth
   </ul>
 </nav>
