7907a81c7d1f4b2c3367d66dd3b24ca484d67613
Keita Nishimura <fast.break.baske03@gmail.com>
2021-03-23 11:29:29 +0900

    feat:記事投稿ページの作成

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -11,7 +11,7 @@
     @endguest
     @auth
     <li class="nav-item">
-      <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+      <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
