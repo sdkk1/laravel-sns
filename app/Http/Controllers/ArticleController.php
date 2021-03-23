7f0331b8e1d5e68c92d36c41ce8d8ae40adc4104
Keita Nishimura <fast.break.baske03@gmail.com>
2021-03-23 16:07:13 +0900

    pref:ArticlePolicyを作成して、記事更新・削除への認可を追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->authorizeResource('App\Article', 'article');
+    }
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
@@ -48,5 +54,5 @@ class ArticleController extends Controller
     public function show(Article $article)
     {
         return view('articles.show', ['article' => $article]);
-    }    
+    }
 }
